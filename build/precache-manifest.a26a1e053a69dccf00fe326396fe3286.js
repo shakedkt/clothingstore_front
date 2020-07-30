3e64e4f7ed1615ad472b8a67c801062cffc1c386
Shaked <shaked@7sjof9ku9h.lan>
2020-07-30 18:00:15 +0300

    added app title

---
+++ build/precache-manifest.a26a1e053a69dccf00fe326396fe3286.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54387f3d6c60cfa66900af74551a1c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d65a8504938dfc985c",
+    "url": "/static/css/main.0270082c.chunk.css"
+  },
+  {
+    "revision": "ba189caf1bc6cecea025",
+    "url": "/static/js/2.1675eee4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1675eee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d65a8504938dfc985c",
+    "url": "/static/js/main.458bdfdc.chunk.js"
+  },
+  {
+    "revision": "1228d0f8e941b5dcef1b",
+    "url": "/static/js/runtime-main.402cc189.js"
+  },
+  {
+    "revision": "9e47f8ae57ada4b0cb78c606a7b37048",
+    "url": "/static/media/borger.9e47f8ae.png"
+  },
+  {
+    "revision": "114f2763b9e1d69b8cfa197becfc05ca",
+    "url": "/static/media/emptybag.114f2763.jpg"
+  },
+  {
+    "revision": "1b85bc592c1c87301f54025f6b0a2316",
+    "url": "/static/media/vegur-bold.1b85bc59.otf"
+  },
+  {
+    "revision": "3bae4005ff2c1863c3942f409f4f4f36",
+    "url": "/static/media/vegur-regular.3bae4005.otf"
+  }
+]);
